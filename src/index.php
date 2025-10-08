102db61f0cbc4b115b21bd32a5b5f6ae8c50a6e0
Ange290 <uange209@gmail.com>
2025-10-08 20:41:16 +0200

    Insert data in DB

--- src/index.php
+++ src/index.php
@@ -14,11 +14,13 @@
 
   </div>
   <form action="#" method="POST" class="w-full max-w-1/2 mx-auto mt-10 p-6 border border-lime-300 rounded-lg shadow-lg font-bold">
+  <label for="id">Problem ID:</label><br>
+  <input type="text" id="id" name="Problem_ID" required class="border border-lime-300 rounded-lg h-12 w-full px-3"><br><br>
     <label for="title">Problem Title:</label><br>
-  <input type="text" id="title" name="title" required class="border border-lime-300 rounded-lg h-12 w-full px-3"><br><br>
+  <input type="text" id="title" name="Problem_Title" required class="border border-lime-300 rounded-lg h-12 w-full px-3"><br><br>
     <label for="issue">Describe your issue:</label><br>
-  <textarea id="issue" name="issue" rows="4" required class="border border-lime-300 rounded-lg w-full px-3 "></textarea> <br>
-    <button type="submit" class="text-white hover:bg-black hover:text-lime-300 bg-lime-700  px-10 py-4 rounded-lg">Submit</button>
+  <textarea id="issue" name="Problem_Desc" rows="4" required class="border border-lime-300 rounded-lg w-full px-3 "></textarea> <br>
+    <button type="submit" class="text-white hover:bg-black hover:text-lime-300 bg-lime-700  px-10 py-4 rounded-lg" name="submit">Submit</button>
   </form>
 </body>
 </html>
@@ -29,8 +31,18 @@ $user = 'root';
 $password = '';
 $dbname = 'it_helpdesk_tickets';
 $conn = new mysqli($host, $user, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}else{
-  echo"Connected successfully";
+
+if(isset($_POST["submit"])){
+  $pb_id = $_POST["Problem_ID"];
+  $pb_tittle = $_POST["Problem_Title"];
+  $pb_desc = $_POST["Problem_Desc"];
+
+  $insert = "INSERT INTO it_issues (Problem_ID, Problem_Title, Problem_Desc) VALUES ('$pb_id', '$pb_tittle', '$pb_desc')";
+  $result = mysqli_query($conn, $insert);
+  if($result){
+    echo "<script>alert('Data Inserted Successfully')</script>";
+  }else{
+    echo "<script>alert('Data Not Inserted')</script>";
+  }
+
 }
